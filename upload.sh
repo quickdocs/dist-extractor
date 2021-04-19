41c3c7054c23cfa225ce343b51b729683bc4a756
Eitaro Fukamachi <e.arrows@gmail.com>
2021-04-20 08:56:03 +0900

    Re-fix upload.sh.

--- upload.sh
+++ upload.sh
@@ -39,9 +39,9 @@ upload_if_changed() {
 
 cd "$directory"
 for entry in `ls $dist`; do
-  if [ -f $entry ]; then
-    upload_if_changed $entry
-  elif [ -d $entry ]; then
+  if [ -f "$dist/$entry" ]; then
+    upload_if_changed "$dist/$entry"
+  elif [ -d "$dist/$entry" ]; then
     version=$entry
     for file in `find $dist/$version -maxdepth 1 -not -type d`; do
       upload_if_changed $file
