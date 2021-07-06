be55a89ad1b2e222967fcba301222a2d364fbe7f
Eitaro Fukamachi <e.arrows@gmail.com>
2021-07-06 09:19:14 +0900

    Redirect standard-output and trace-output to stderr while running asdf:load-asd.
    Some libraries prints something to stdout in ASD files.

--- lib/dependencies.lisp
+++ lib/dependencies.lisp
@@ -90,7 +90,9 @@
         (with-autoload-on-missing
           (let ((*macroexpand-hook* (make-hook *macroexpand-hook* system-file)))
             (handler-bind ((warning #'muffle-warning))
-              (asdf:load-asd system-file))))))
+              (let ((*standard-output* *error-output*)
+                    (*trace-output* *error-output*))
+                (asdf:load-asd system-file)))))))
     (mapcar (lambda (system-file)
               (let ((value (gethash system-file *registry*)))
                 (cons system-file (nreverse value))))
