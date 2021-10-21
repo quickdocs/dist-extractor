050b044caa37493476a6bcf9be013ed12e553c4b
Eitaro Fukamachi <e.arrows@gmail.com>
2021-10-21 02:00:15 +0000

    Fix the regex in the workaround which is added at 408d383e3cd1594858626b819808bcda4cc36972.

--- extract.sh
+++ extract.sh
@@ -39,7 +39,7 @@ else
 
   # KLUDGE: The directory name may different from the dist version (ex. '2021-10-21')
   if [ "${#releases[@]}" == 0 ]; then
-    releases=( $(cat "$destination/$dist/$version/releases.json" | jq -r "to_entries | map(select(.value | scan(\"[0-9]{4}-[0-9]{2}\") == \"$(echo $version | cut -d - -f -2)-\")) | map(.key) | .[]") )
+    releases=( $(cat "$destination/$dist/$version/releases.json" | jq -r "to_entries | map(select(.value | scan(\"[0-9]{4}-[0-9]{2}-\") == \"$(echo $version | cut -d - -f -2)-\")) | map(.key) | .[]") )
   fi
 
   echo "Extracting new/updated projects in $version."
